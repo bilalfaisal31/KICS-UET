f0673abac79eaa8bbd44e254275e789ed1ed22ba
bilalfaisal31 <bilal.faisal31@gmail.com>
2021-10-07 11:06:17 +0500

    Update ageCalculator .php

--- Assignments/10 PHP calculators/ageCalculator .php
+++ Assignments/10 PHP calculators/ageCalculator .php
@@ -1,43 +1,8 @@
-<?php
 
-if(isset($_POST['sub'])){
-
-	$mm=$_POST['mm'];
-
-	$dd=$_POST['dd'];
-
-	$yy=$_POST['yy'];
-
-	$dob=$mm."/".$dd."/".$yy;
-
-	$arr=explode('/',$dob);
-
-	//$dateTs=date_default_timezone_set($dob); 
-	$dateTs=strtotime($dob);
-
-	$now=strtotime('today');
-
-	if(sizeof($arr)!=3) die('ERROR:please entera valid date');
-
-	if(!checkdate($arr[0],$arr[1],$arr[2])) die('PLEASE: enter a valid dob');
-
-	if($dateTs>=$now) die('ENTER a dob earlier than today');
-
-	$ageDays=floor(($now-$dateTs)/86400);
-
-	$ageYears=floor($ageDays/365);
-
-	$ageMonths=floor(($ageDays-($ageYears*365))/30);
-
-	echo "<font color='red' size='10'> You are aprox $ageYears years and $ageMonths months old.  </font>";
-
-}
-
-?>
-
-<form method="post"><center>
+<form method="post">
+    <center>
 	
-Choose your DOB
+    Choose your DOB
 
 	<select name="yy">
 		<option value="">Year</option>
@@ -49,7 +14,7 @@ Choose your DOB
 		?>
 	</select>
 	
-<select name="mm">
+    <select name="mm">
 		<option value="">Month</option>
 		<?php
 		for($i=1;$i<=12;$i++)
@@ -70,8 +35,31 @@ Choose your DOB
 
 		?>
 	</select>
-<input type="submit" name="sub" value="check it"/>
+    <input type="submit" name="sub" value="check it"/>
 
-	</center>
+    </center>
 
-	</form>
+</form>
+
+<center>
+<?php
+
+if(isset($_POST['sub'])){
+
+    $mm=$_POST['mm'];
+    $dd=$_POST['dd'];
+    $yy=$_POST['yy'];
+
+    $dob=$mm."-".$dd."-".$yy;
+
+    if( strlen($dob) >=8 ){
+        $today = date("Y-m-d");
+        $diff = date_diff(date_create($dob), date_create($today));
+    
+        echo 'You are '.$diff->format('%y'). ' years old';
+    }else{
+        echo "Select your DOB first Year,Month and Date";
+    }
+}
+?>
+</center>
